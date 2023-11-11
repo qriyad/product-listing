4be6654276831f97da90e53436dc72834f55f8e9
capybara31 <riyadqhrmanli@gmail.com>
2023-11-11 18:56:25 +0400

    theme switcher error solved

--- frontend/front/src/pages/Auth/Login.jsx
+++ frontend/front/src/pages/Auth/Login.jsx
@@ -1,23 +1,33 @@
 import { Card, Flex, Input, Button, Space, Typography, Switch } from 'antd';
 import React, { useState } from 'react';
-import MoonIcon from '../../components/icons/MoonIcon'
-import SunIcon from '../../components/icons/SunIcon'
-import SwitchIcon from '../../components/Switch'
+import MoonIcon from '../../components/icons/MoonIcon';
+import SunIcon from '../../components/icons/SunIcon';
+import usersData from '../people.json';
 
 const boxStyle = {
-  width: "100%",
-  height: "100vh",
-  backgroundColor: "lightgrey",
-  backgroundSize: "cover",
+  width: '100%',
+  height: '100vh',
+  backgroundSize: 'cover',
+  transition: 'background-color 0.3s',
+};
+
+const darkTheme = {
+  backgroundColor: '#333',
+  color: '#fff',
+};
+
+const lightTheme = {
+  backgroundColor: 'lightgrey',
+  color: 'black',
 };
 
 const fakeAuthentication = async (email, password) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (email === 'user@example.com' && password === 'userpassword') {
-        resolve({ role: 'user' });
-      } else if (email === 'admin@example.com' && password === 'adminpassword') {
-        resolve({ role: 'admin' });
+      const user = usersData.users.find((u) => u.email === email && u.password === password);
+
+      if (user) {
+        resolve({ role: user.role });
       } else {
         reject(new Error('Invalid credentials'));
       }
@@ -28,6 +38,7 @@ const fakeAuthentication = async (email, password) => {
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [theme, setTheme] = useState('light'); // Default theme is light
 
   const handleLogin = async () => {
     try {
@@ -38,36 +49,42 @@ const Login = () => {
     }
   };
 
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
   return (
-      <Flex style={boxStyle} align="center" justify="center">
-        <Card title="Login" bordered={false} style={{ width: 500 }}>
-          <Space size="small" direction="vertical" style={{ width: '100%' }}>
-            <Input
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <Input
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <Flex justify="space-between" align="center">
-              <Button type="primary" onClick={handleLogin}>
-                Login
-              </Button>
-              <MoonIcon />
-              <SwitchIcon />
-              <SunIcon />
-              <Typography.Link>Forgot Password?</Typography.Link>
-            </Flex>
-          </Space>
-        </Card>
-      </Flex>
+    <Flex style={{ ...boxStyle, ...(theme === 'dark' ? darkTheme : lightTheme) }} align="center" justify="center">
+      <Card title="Login" bordered={false} style={{ width: 500 }}>
+        <Space size="small" direction="vertical" style={{ width: '100%' }}>
+          <Input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            style={{ background: 'transparent' }}
+          />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={{ background: 'transparent' }}
+          />
+          <Flex justify="space-between" align="center">
+            <Button type="primary" onClick={handleLogin}>
+              Login
+            </Button>
+            <SunIcon />
+            <Switch checked={theme === 'dark'} onChange={toggleTheme} />
+            <MoonIcon />
+            <Typography.Link>Forgot Password?</Typography.Link>
+          </Flex>
+        </Space>
+      </Card>
+    </Flex>
   );
 };
 
--- frontend/front/src/pages/Auth/Register.jsx
+++ frontend/front/src/pages/Auth/Register.jsx
@@ -1,30 +1,74 @@
-import { Card, Flex, Input, Button, Space, Typography } from 'antd';
-import React from 'react'
+import { Card, Flex, Input, Button, Space } from 'antd';
+import React, { useState } from 'react';
+import usersData from '../people.json'; // Import the JSON file
 
 const boxStyle = {
-    width: "100%",
-    height: "100vh",
-    backgroundColor: "lightgrey",
-    backgroundSize: "cover",
+  width: '100%',
+  height: '100vh',
+  backgroundColor: 'lightgrey',
+  backgroundSize: 'cover',
 };
 
-const Login = () => {
+const isUserRegistered = (email) => {
+  return usersData.users.some((user) => user.email === email);
+};
+
+const registerUser = (userData) => {
+  usersData.users.push(userData);
+  // Save the updated user data back to the JSON file or your backend/database
+  // For simplicity, in this example, we are not saving it back to the file.
+  console.log('User registered:', userData);
+};
+
+const Registration = () => {
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleRegister = () => {
+    if (isUserRegistered(email)) {
+      console.error('User already registered with this email.');
+    } else {
+      if (password !== confirmPassword) {
+        console.error('Password and confirm password do not match.');
+      } else {
+        const userData = {
+          name,
+          surname,
+          email,
+          password,
+          role: 'user', // You can set a default role for registered users
+        };
+        registerUser(userData);
+      }
+    }
+  };
+
   return (
     <Flex style={boxStyle} align="center" justify="center">
-        <Card title="Login" bordered={false} style={{ width: 500 }}>
-        <Space size="small" direction="vertical" style={{ width: "100%" }}>
-            <Input type="text" name='name' placeholder="Name" />
-            <Input type="text" name='surname' placeholder="Surname" />
-            <Input type="email" name='email' placeholder="Email" />
-            <Input type="password" name='password' placeholder="Password" />
-            <Input type="password" name='confirm_password' placeholder="Confirm Password" />
-            <Flex justify='space-between' align='center'>
-                <Button type="primary">Register</Button>
-            </Flex>
+      <Card title="Register" bordered={false} style={{ width: 500 }}>
+        <Space size="small" direction="vertical" style={{ width: '100%' }}>
+          <Input type="text" name="name" placeholder="Name" onChange={(e) => setName(e.target.value)} />
+          <Input type="text" name="surname" placeholder="Surname" onChange={(e) => setSurname(e.target.value)} />
+          <Input type="email" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+          <Input type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+          <Input
+            type="password"
+            name="confirm_password"
+            placeholder="Confirm Password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <Flex justify="space-between" align="center">
+            <Button type="primary" onClick={handleRegister}>
+              Register
+            </Button>
+          </Flex>
         </Space>
-        </Card>
+      </Card>
     </Flex>
-  )
-}
+  );
+};
 
-export default Login
+export default Registration;
